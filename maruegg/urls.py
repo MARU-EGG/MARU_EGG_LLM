14bfe5ea2ce5c1d5fbdeb3bd94672b724e640728
choieuntaek <choiet0225@gmail.com>
2024-07-09 14:09:08 +0900

    feat: test api develop

--- maruegg/urls.py
+++ maruegg/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("upload_html/", views.upload_html, name="upload_html"),
     path("ask_question_api/", views.ask_question_api, name="ask_question_api"),
     path("ask_question_model/", views.ask_question_model, name="ask_question_model"),
+    path('test/', views.test, name='test'),
+    path('api_ask_question/', views.api_ask_question, name='api_ask_question'),  # 새로운 API URL 추가
 ]
