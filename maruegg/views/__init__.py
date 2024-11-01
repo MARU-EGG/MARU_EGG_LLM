642d5c058c67682b4e06ddb4cfe28a25be67ccfd
choieuntaek <choiet0225@gmail.com>
2024-11-01 14:20:41 +0900

    feat: prompt managing system & swagger security

--- maruegg/views/__init__.py
+++ maruegg/views/__init__.py
@@ -3,3 +3,4 @@ from .retrieve_views import retrieve_documents
 from .main_llm_views import ask_question_api
 from .main_views import main
 from .pdf_upload_views import upload_pdf
+from .prompt_views import create_prompt, get_prompt, update_prompt, delete_prompt
